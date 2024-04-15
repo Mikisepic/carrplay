2d0729cf27f0ebb50f66cd54f7259e2a4eb13e97
Mikas Pupeikis <mikas.pup@gmail.com>
2024-04-15 18:36:09 +0000

    feat(services): add car plate service

--- server/Models/CarPlate.cs
+++ server/Models/CarPlate.cs
@@ -3,6 +3,6 @@ namespace server.Models;
 public class CarPlate
 {
     public Guid Id { get; set; }
-    public Guid UserId { get; }
+    public Guid UserId { get; set; }
     public string? Plate { get; set; }
 }
---
+++ server/Services/CarPlateService.cs
@@ -0,0 +1,42 @@
+using server.Models;
+
+namespace server.Services;
+
+public static class CarPlateService
+{
+    static List<CarPlate> CarPlates { get; }
+    static CarPlateService()
+    {
+        CarPlates = new List<CarPlate> {
+            new CarPlate {Id = Guid.NewGuid(), UserId = Guid.NewGuid(), Plate = "QWE-123"},
+            new CarPlate {Id = Guid.NewGuid(), UserId = Guid.NewGuid(), Plate = "ABC-124"},
+        };
+    }
+
+    public static List<CarPlate> GetAll() => CarPlates;
+
+    public static CarPlate? Get(Guid id) => CarPlates.FirstOrDefault(carPlate => carPlate.Id == id);
+
+    public static void Add(CarPlate carPlate)
+    {
+        CarPlates.Add(carPlate);
+    }
+
+    public static void Delete(Guid id)
+    {
+        var carPlate = Get(id);
+        if (carPlate is null)
+            return;
+
+        CarPlates.Remove(carPlate);
+    }
+
+    public static void Update(CarPlate carPlate)
+    {
+        var index = CarPlates.FindIndex(plate => carPlate.Id == plate.Id);
+        if (index == -1)
+            return;
+
+        CarPlates[index] = carPlate;
+    }
+}
