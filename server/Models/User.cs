a588a62e26279a39bff9acdfa2fba350aab8b1d7
Mikas Pupeikis <mikas.pup@gmail.com>
2024-04-15 18:40:51 +0000

    feat(models): add user model

---
+++ server/Models/User.cs
@@ -0,0 +1,10 @@
+namespace server.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+}
