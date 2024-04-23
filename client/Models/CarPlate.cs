924659a79da2e0089e7d23004fe315173d54aabd
Mikas Pupeikis <mikas.pup@gmail.com>
2024-04-23 19:23:55 +0000

    feat(plates): format licenses

--- client/Models/CarPlate.cs
+++ client/Models/CarPlate.cs
@@ -1,11 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace client.Models;
 
-public class CarPlate
+public class Plate
 {
     public Guid Id { get; set; }
     public string PlateString { get; set; } = "";
     public DateTime CreatedAt { get; set; }
     public string Country { get; set; } = "";
+    public string GetFormattedLicense() => $"{Country}-{PlateString}";
 }
