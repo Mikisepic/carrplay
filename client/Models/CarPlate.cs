1bbf4489dd4d4504d5d192fa51d01dac57d2c630
Mikas Pupeikis <mikas.pup@gmail.com>
2024-04-23 19:05:26 +0000

    feat(plates): add plates v1

--- client/Models/CarPlate.cs
+++ client/Models/CarPlate.cs
@@ -5,7 +5,7 @@ namespace client.Models;
 public class CarPlate
 {
     public Guid Id { get; set; }
-    public string Plate { get; set; }
+    public string PlateString { get; set; } = "";
     public DateTime CreatedAt { get; set; }
-    public string Country { get; set; }
+    public string Country { get; set; } = "";
 }
